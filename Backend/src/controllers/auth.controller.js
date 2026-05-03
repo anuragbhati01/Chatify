ae9c4f9331e69f442eeabc76991a1a0d3ba02e00
anuragbhati01 <anuragbhatiofficial@gmail.com>
2026-05-03 20:11:04 +0530

    Sign up endpoint done

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,58 @@
+import { generateToken } from "../lib/utils.js";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+export const signup = async (req , res) => {
+    const {fullName, email, password} = req.body;
+
+    try {
+        if(!fullName || !email || !password){
+            res.status(400).json({message : "All fields are required!"});
+        }
+
+        if(password.length < 6){
+            res.status(400).json({message: "password length should be greater than 6"});
+        }
+
+        // Email validation 
+        // Regular expression for validating email format
+
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if(!emailRegex.test(email)){
+            res.status(400).json({message: "Email is not valid"});
+        }
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser) return res.status(400).json({message: "Email already registered"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        if(newUser){
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id, res);
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        }
+        else{
+            res.status(400).json({message: "Invalid user data"});
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller : ", error)
+        res.status(500).json({message: "Server error", error: error.message});
+    }
+}
---
+++ Backend/src/lib/utils.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+    const {JWT_SECRET, NODE_ENV} = process.env;
+    if(!JWT_SECRET){
+        throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+    // jwt.sign(payload, secret, options)
+
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn : "7d"});
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true, // prevents XSS attacks: cross-site scripting
+        sameSite: "strict", // prevents CSRF attacks
+        secure: process.env.NODE_ENV === "development" ? false : true,
+    });
+
+    return token;
+}
+
