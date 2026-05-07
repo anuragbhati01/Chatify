2d29373850d792491866b2ed09ed8075cb09d0a4
anuragbhati01 <anuragbhatiofficial@gmail.com>
2026-05-07 20:07:49 +0530

    welcome email implemeted

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import { generateToken } from "../lib/utils.js";
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import "dotenv/config"
+import { ENV } from "../lib/env.js";
 
 export const signup = async (req , res) => {
     const {fullName, email, password} = req.body;
@@ -46,6 +49,13 @@ export const signup = async (req , res) => {
                 email: newUser.email,
                 profilePic: newUser.profilePic,
             });
+
+            // Sending welcome message
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL)
+            } catch (error) {
+                console.error("Error sending welcome email: ", error);
+            }
         }
         else{
             res.status(400).json({message: "Invalid user data"});
