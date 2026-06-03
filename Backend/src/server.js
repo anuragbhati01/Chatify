7bcc4783dd05387dc09ffd241e01794c91cbc924
anuragbhati01 <anuragbhatiofficial@gmail.com>
2026-06-03 10:13:21 +0530

    signup implemented for frontend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js';
@@ -13,7 +14,8 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json()); //
+app.use(express.json()); 
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
