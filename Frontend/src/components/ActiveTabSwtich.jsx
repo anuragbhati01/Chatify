1761ba4cc925aa561e37cee2e72d25d97928b93f
anuragbhati01 <anuragbhatiofficial@gmail.com>
2026-06-09 19:22:20 +0530

    sidebar completed

--- Frontend/src/components/ActiveTabSwtich.jsx
+++ Frontend/src/components/ActiveTabSwtich.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useChatStore } from "../store/useChatStore.js";
 
 function ActiveTabSwtich() {
+  const { activeTab, setActiveTab } = useChatStore();
+
   return (
-    <div>
-      Active tab switch
+    <div className="tabs tabs-box bg-transparent py-2 pr-6 m-2 w-full">
+      <button
+        onClick={() => setActiveTab("chats")}
+        className={`tab transition-colors  w-1/2 font-medium ${
+          activeTab === "chats"
+            ? "bg-cyan-500/20  text-cyan-400 "
+            : "text-slate-400"
+        }`}
+      >
+        Chats
+      </button>
+
+      <button
+        onClick={() => setActiveTab("contacts")}
+        className={`tab transition-colors w-1/2 font-medium ${
+          activeTab === "contacts"
+            ? "bg-cyan-500/20 text-cyan-400  "
+            : "text-slate-400"
+        }`}
+      >
+        Contacts
+      </button>
     </div>
-  )
+  );
 }
 
-export default ActiveTabSwtich
+export default ActiveTabSwtich;
--- Frontend/src/components/ContactList.jsx
+++ Frontend/src/components/ContactList.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore.js";
+import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
 
 function ContactList() {
+  const { getAllContacts, allContacts, isUserLoading, setSelectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+
+  if (isUserLoading) return <UsersLoadingSkeleton />;
+
   return (
-    <div>
-      Contact List
-    </div>
-  )
+    <>
+      {allContacts.map((contact) => (
+        <div
+          key={contact._id}
+          className="bg-cyan-500/10 hover:bg-cyan-500/20 p-4 rounded-lg cursor-pointer transition-colors"
+          onClick={() => setSelectedUser(contact)}
+        >
+          <div className="flex items-center gap-3">
+            <div className={`avatar avatar-online`}>
+              <div className="size-12 rounded-full">
+                <img
+                  src={contact.profilePic || "/avatar.png"}
+                  alt={contact.fullName}
+                />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">
+              {contact.fullName}
+            </h4>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default ContactList
+export default ContactList;
